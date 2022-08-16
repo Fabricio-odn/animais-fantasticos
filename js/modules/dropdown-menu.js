e3c5a5653f2c5484db880955e4772468d85c9960
Fabricio Oliveira <fabriciodeoliveira334@gmail.com>
2022-08-15 22:53:55 -0300

    adicionado slide de fotos

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,31 +1,30 @@
-import outsideClick from "./outsideclick.js";
+import outsideClick from './outsideclick.js';
 
 export default class DropdownMenu {
   constructor(dropdownMenus, events) {
     this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-    // Define argumentos padrão de events caso não tenha sido definido
-    if (events === undefined) {
-      this.events = ["touchstart", "click"];
-    } else {
-      this.events = events;
-    }
-    this.activeClass = "active";
+    // define touchstart e click como argumento padrão
+    // de events caso o usuário não define
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
 
+    this.activeClass = 'active';
     this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
   }
 
-  // Ativa o dropdownmenu e adiciona a funcão que observa o click fora dele
+  // Ativa o dropdownmenu e adiciona
+  // a função que observa o clique fora dele
   activeDropdownMenu(event) {
     event.preventDefault();
     const element = event.currentTarget;
     element.classList.add(this.activeClass);
     outsideClick(element, this.events, () => {
-      element.classList.remove(this.activeClass);
+      element.classList.remove('active');
     });
   }
 
-  // Adiciona os eventos ao dropdownmenu
+  // adiciona os eventos ao dropdownmenu
   addDropdownMenusEvent() {
     this.dropdownMenus.forEach((menu) => {
       this.events.forEach((userEvent) => {
--- js/modules/modal.js
+++ js/modules/modal.js
@@ -5,7 +5,7 @@ export default class Modal {
     this.containerModal = document.querySelector(containerModal);
 
     // bind this ao callback para
-    // fazer referencia ao objeto
+    // fazer referência ao objeto
     // da classe
     this.eventToggleModal = this.eventToggleModal.bind(this);
     this.cliqueForaModal = this.cliqueForaModal.bind(this);
@@ -13,7 +13,7 @@ export default class Modal {
 
   // abre ou fecha o modal
   toggleModal() {
-    this.containerModal.classList.toggle("ativo");
+    this.containerModal.classList.toggle('ativo');
   }
 
   // adiciona o evento de toggle ao modal
@@ -25,20 +25,20 @@ export default class Modal {
   // fecha o modal ao clicar do lado de fora
   cliqueForaModal(event) {
     if (event.target === this.containerModal) {
-      this.toggleModal(event);
+      this.toggleModal();
     }
   }
 
   // adiciona os eventos aos elementos do modal
-  addModaEvent() {
-    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
-    this.botaoFechar.addEventListener("click", this.eventToggleModal);
-    this.containerModal.addEventListener("click", this.cliqueForaModal);
+  addModalEvents() {
+    this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventToggleModal);
+    this.containerModal.addEventListener('click', this.cliqueForaModal);
   }
 
   init() {
     if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
-      this.addModaEvent();
+      this.addModalEvents();
     }
     return this;
   }
